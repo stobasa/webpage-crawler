5d1903c5f731edd24da028739579d895d07ef444
pepps01 <slpepple01@gmail.com>
2020-06-22 05:01:27 -0500

    Rerouted Crawler Urls to access on a yearly basis when requested

--- root/urls.py
+++ root/urls.py
@@ -34,7 +34,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	path('api/v1/', include('crawler.urls')),
+	path('', include('crawler.urls')),
 	re_path(r'^(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 	re_path(r'^$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
