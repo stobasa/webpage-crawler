f8f96447e1d7c211322035c67d91440fd37ccbf3
unknown <gbubemimakpokpomi@gmail.com>
2020-07-01 18:35:52 +0100

    daily-payment-report

--- crawler/views.py
+++ crawler/views.py
@@ -275,3 +275,40 @@ def year_2020_download(request):
 								result.append({table_data[0].text: ""})
 		print("The end")
 		return Response([result])
+
+
+@api_view(['GET'])
+def daily_report(request, year, month, date):
+	"""
+	Daily report view
+	:param request: Year, Month, Date
+	:return: the download link for the particular day
+	"""
+	try:
+		if int(year) == 2018:
+			url = f'{homepage + querystring_2018}'
+		if int(year) == 2019:
+			url = f'{homepage + querystring_2019}'
+		if int(year) == 2020:
+			url = f'{homepage + querystring_2020}'
+		
+		if request.method == 'GET':
+			r = requests.get(url, verify=False).text
+			soup = BeautifulSoup(r, 'lxml')
+			for section in soup.find('section', attrs={'class': 'sppb-section'}):
+				for div in section.find_all('div', attrs={'class': 'sppb-panel'})[int(month) - 1]:
+					table = div.find_all('table')
+					
+					for table_rows in table:
+						for tr in table_rows.find_all("tr")[int(date) : int(date)+1]:
+							table_data = tr.find_all('td')
+							for i in table_data:
+								if i != None:
+									try:
+										result = {table_data[0].text: homepage + table_data[1].find('a')['href']}
+									except:
+										result = {table_data[0].text: ""}
+			return Response([result])
+		
+	except:
+		return Response("Invaid date")
