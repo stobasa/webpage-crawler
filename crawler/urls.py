25eac72bdf4b070e02864b38eb6352e974a75229
pepps01 <slpepple01@gmail.com>
2020-07-03 14:12:59 +0100

    Added the Cron Job and Treasury Statement

--- crawler/urls.py
+++ crawler/urls.py
@@ -8,8 +8,14 @@ urlpatterns = [
 	path('2018/monthly/', year_2018_momthly),
 	path('2019/monthly/', year_2019_momthly),
 	path('2020/monthly/', year_2020_momthly),
+	path('2018/treasury/monthly', treasury_2018),
+	path('2019/treasury/monthly', treasury_2019),
+	path('2020/treasury/monthly', treasury_2020),
+
 	path('monthlyreport/<int:year>/<str:based>/<str:month>/', monthly_report),
 	re_path(r'^dailyreport/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<date>[0-9]{1,2})/$', daily_report),
 	re_path(r'^dailyreportjson/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<date>[0-9]{1,2})/$', daily_report_json),
+	re_path(r'^dailytreasuryreport/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<date>[0-9]{1,2})/$', daily_report),
+
 	path('cron/', cronjob),
 ]
